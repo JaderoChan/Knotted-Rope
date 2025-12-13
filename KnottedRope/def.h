855a843207e156f90b100aea7df98d588cdaf21c
JaderoChan <c_dl_cn@outlook.com>
2025-12-13 22:28:04 +0800

    Update the hard-coded path

--- KnottedRope/def.h
+++ KnottedRope/def.h
@@ -2,11 +2,11 @@
 
 constexpr const char* APP_TITLE = "Knotted Rope";
 constexpr const char* APP_CONFIG_FILENAME = ".config";
-constexpr const char* APP_LANGUAGES_FILENAME = "./language/languages.json";
-constexpr const char* APP_LOCK_FILENAME = "Lock-KnottedRope-24f3b3a0-767c-47e9-9949-f55c3d9dfec7";
+constexpr const char* APP_LANGUAGES_FILENAME = "language/languages.json";
+constexpr const char* APP_LOCK_FILENAME = ".Lock-KnottedRope-24f3b3a0-767c-47e9-9949-f55c3d9dfec7";
 constexpr const char* APP_ASSOCIATED_FILEEXT = ".kr";
 
-constexpr const char* DEFAULT_STORE_PATH = "./knotted_ropes";
+constexpr const char* DEFAULT_STORE_PATH = "knotted_ropes";
 constexpr const char* DEFAULT_FILENAME_PREFIX = "KnottedRope";
 
 constexpr const int RECENT_FILE_MAXNUM = 15;
